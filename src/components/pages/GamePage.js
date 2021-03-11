5e7fbfc97b7de03a0cfdf9ca575ddef411372acf
pahalce <tkrmineral@gmail.com>
2021-03-11 23:56:40 +0900

    gamepage gameboard

--- src/components/pages/GamePage.js
+++ src/components/pages/GamePage.js
@@ -4,15 +4,20 @@ import Card from "../reusables/Card";
 const GamePage = ({ letter, adj }) => {
   return (
     <div className="gamepage">
-      <div className="theme">
+      <div className="gamepage-theme">
         <span>{letter}</span>
         からはじまる
         <span>{adj}</span>
         言葉
       </div>
-      <div className="gamepage-board">
-        <Card title="山田　太郎" content="考え中" />
+      <div className="gamepage-board ">
+        <Card title="山田　太郎" content="考え中..." width="24vw" height="24vh" fontSize="1.4em" />
+        <Card title="山田　太郎" content="考え中..." width="24vw" height="24vh" fontSize="1.4em" />
+        <Card title="山田　太郎" content="考え中..." width="24vw" height="24vh" fontSize="1.4em" />
+        <Card title="山田　太郎" content="考え中..." width="24vw" height="24vh" fontSize="1.4em" />
+        <Card title="山田　太郎" content="考え中..." width="24vw" height="24vh" fontSize="1.4em" />
       </div>
+      aaa
     </div>
   );
 };
