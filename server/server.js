cb745016d6f54c69d3e2061ae49e29a667243806
pahalce <tkrmineral@gmail.com>
2021-03-27 01:22:36 +0900

    socket server states

--- server/server.js
+++ server/server.js
@@ -9,6 +9,9 @@ io.on("connection", (socket) => {
   const id = socket.handshake.query.id;
   socket.join(id);
   socket.on("add-player", (msg) => {
-    socket.broadcast.emit("r", "msg from server");
+    socket.broadcast.emit("r", msg);
+  });
+  socket.on("change-state", (state) => {
+    socket.broadcast.emit("change-state", state);
   });
 });
